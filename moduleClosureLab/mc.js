1c5a1b247ca1b7c0ef5bbb978a47bd3e4866c5e3
ucguleviz <ugurcan.guleviz@gmail.com>
2022-01-24 01:09:07 -0600

    jQuery Quiz1 and MouseMaze added

--- moduleClosureLab/mc.js
+++ moduleClosureLab/mc.js
@@ -1,36 +1,55 @@
 var accountInfoList = [];
 
+var funcs = [];
+for (var k= 0; k < 5; k++) {
+  funcs[k] = function () {
+      return k; 
+  };
+}
+
+console.log(funcs[0]());
+console.log(funcs[1]());
+console.log(funcs[2]());
+console.log(funcs[3]());
+console.log(funcs[4]());
+
 var Module = (function () {
-    function Account(name, value) {
-        this.accountType = name;
-        this.depositVal = value
+  function Account(name, value) {
+    this.accountType = name;
+    this.depositVal = value;
+  }
+
+  const submit = document.querySelector("#submit");
+  const textArea = document.querySelector("#text");
+
+  submit.addEventListener("click", addAccount);
+
+  function addAccount() {
+    let accountName = document.querySelector("#account");
+    let depositVal = document.querySelector("#deposit");
+    const createdAccount = new Account(accountName.value, depositVal.value);
+    accountInfoList.push(createdAccount);
+    cleanArea();
+    listAccount();
+  }
+
+  function cleanArea() {
+    textArea.value = "";
+  }
+
+  function listAccount() {
+    for (const account of accountInfoList) {
+      textArea.value +=
+        "Account Name: " +
+        account.accountType +
+        " " +
+        "Balance: " +
+        account.depositVal +
+        "\r\n";
     }
+  }
 
-    const submit = document.querySelector('#submit');
-    const textArea = document.querySelector('#text');
-
-    submit.addEventListener('click', addAccount);
-
-    function addAccount() {
-        let accountName = document.querySelector('#account');
-        let depositVal = document.querySelector('#deposit');
-        const createdAccount = new Account(accountName.value, depositVal.value);
-        accountInfoList.push(createdAccount);
-        cleanArea();
-        listAccount();
-    }
-
-    function cleanArea() {
-        textArea.value = '';
-    }
-
-    function listAccount() {
-        for(const account of accountInfoList) {
-            textArea.value += 'Account Name: ' +  account.accountType + ' ' + 'Balance: '  + account.depositVal +  '\r\n';
-        }
-    }
-
-    return {
-        addBankAccount: addAccount
-    };
+  return {
+    addBankAccount: addAccount,
+  };
 })();
